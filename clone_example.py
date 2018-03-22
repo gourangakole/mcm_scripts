3fb729c1bbca95a5330a7d8fdff42d9b6cc1eafc
Gouranga Kole <gouranga.tifr@gmail.com>
2018-03-22 10:16:25 +0100

    little modificaton

--- clone_example.py
+++ clone_example.py
@@ -1,17 +1,26 @@
 import sys
 sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
 
+#---gkole start
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--foo', help='foo help')
+#args = parser.parse_args()
+#----gkole end
+print "Number of arguments: ", len(sys.argv)
+print "The arguments are: " , str(sys.argv)
 from rest import *
 
-mcm = restful(dev=True)
+mcm = restful(dev=False) #it was True
 
-
+print sys.argv[1]
 # script clones a request to other campaign.
 # define list modifications, if member_of_campaign is different
 # it will clone to other campaign
 
-modif = {'extension' : 1, 'total_events' : 101, 'member_of_campaign': 'Summer12'}
-__req_prepid = "SUS-RunIIWinter15wmLHE-00040"
+
+modif = {'extension' : 1, 'total_events' : sys.argv[1]}
+__req_prepid = "HIG-RunIIFall17wmLHEGS-01355"
 
 # get a request object which we want to clone
 a_request = mcm.getA('requests', __req_prepid)
